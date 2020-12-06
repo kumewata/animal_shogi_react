466528a15b04b8730668f91d574d97be477fb0a9
Wataru Kume <kumewata@gmail.com>
2020-12-06 20:28:22 +0900

    introduce to display moving candidates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,113 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './App.css';
 
 class Square extends React.Component {
   render() {
+    let className = 'square';
+    if (this.props.isSelected) {
+      className += ' selected';
+    }
+    if (this.props.isCandidate) {
+      className += ' candidate';
+    }
     return (
-      <button className="square">
-        {/* TODO */}
+      <button
+        className={className}
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.type}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    const squares = [
+      {
+        type: '0,0',
+        position: [0, 0],
+      },
+      {
+        type: '1,0',
+        position: [1, 0],
+      },
+      {
+        type: '2,0',
+        position: [2, 0],
+      },
+      {
+        type: '0,1',
+        position: [0, 1],
+      },
+      {
+        type: '1,1',
+        position: [1, 1],
+      },
+      {
+        type: '2,1',
+        position: [2, 1],
+      },
+      {
+        type: '0,2',
+        position: [0, 2],
+      },
+      {
+        type: '1,2',
+        position: [1, 2],
+      },
+      {
+        type: '2,2',
+        position: [2, 2],
+      },
+      {
+        type: '0,3',
+        position: [0, 3],
+      },
+      {
+        type: '1,3',
+        position: [1, 3],
+      },
+      {
+        type: '2,3',
+        position: [2, 3],
+      },
+    ]
+    this.state = {
+      squares: squares,
+      selectedSquareIndex: null,
+      movingCandidates: [],
+    }
+  }
+
   renderSquare(i) {
-    return <Square />;
+    const position = this.state.squares[i].position;
+    const samePosition = (element) => element[0] === position[0] && element[1] === position[1];
+    return (
+      <Square
+        type={this.state.squares[i].type}
+        onClick={() => this.handleClick(i)}
+        isSelected={this.state.selectedSquareIndex === i}
+        isCandidate={this.state.movingCandidates.some(samePosition)}
+      />);
+  }
+
+  handleClick(i) {
+    if (this.state.selectedSquareIndex === i) {
+      this.setState({
+        selectedSquareIndex: null,
+        movingCandidates: []
+      });
+    } else {
+      this.setState({selectedSquareIndex: i});
+      const position = this.state.squares[i].position
+      const diffs = movingDiffs();
+      const candidates = diffs.map(diff => [diff[0] + position[0], diff[1] + position[1]]);
+      const filteredCandidates = candidates.filter(c => c[0] >= 0 && c[0] < 3 && c[1] >= 0 && c[1] < 4);
+      this.setState({movingCandidates: filteredCandidates});
+    }
   }
 
   render() {
@@ -62,6 +155,10 @@ class App extends React.Component {
       </div>
     );
   }
+}
+
+function movingDiffs() {
+  return [[0, -1]];
 }
 
 export default App;
